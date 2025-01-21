2a3556dc4403b6c51923e696a6d82113e4e0edb0
Cole Essig <hoopwalker@yahoo.com>
2025-01-20 23:35:53 -0700

    added more styles, corrected header for mobile, changed up some display logic

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,15 @@
 import './NavBar.css';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useContext } from "react";
 import { CurrentUserContext } from "../../utils/contexts/CurrentUserContext";
 
 function NavBar({ isLoggedIn, signOut, className, isHome, signInClick }) {
     const currentUser = useContext(CurrentUserContext);
+    const [isHovered, setIsHovered] = useState(false);
+    const handleMouseEnter = () => setIsHovered(true);
+    const handleMouseLeave = () => setIsHovered(false);
+    
     const handleLogout = (e) => {
       e.preventDefault();
       signOut();
@@ -12,6 +17,10 @@ function NavBar({ isLoggedIn, signOut, className, isHome, signInClick }) {
 
     const handleSignIn = () => {
     signInClick();
+    }
+
+    const handleMobileClick = () => {
+      setIsHovered(!isHovered)
     }
 
     return(
@@ -23,22 +32,37 @@ function NavBar({ isLoggedIn, signOut, className, isHome, signInClick }) {
                 </div>
                 {!isLoggedIn ?
                   <div className='nav__dynamic-links'>
-                    <Link to='/' className='nav__home-link'>
+                    <Link 
+                      to='/' 
+                      className={`nav__home-link`}
+                    >
                       Home
                     </Link>
-                    <button type='button' className='nav__button' onClick={handleSignIn} >Sign In</button>
+                    <button 
+                      type='button' 
+                      className={`nav__button`} 
+                      onClick={handleSignIn} 
+                    >Sign In</button>
                   </div>
                   :
                   <div className='nav__dynamic-links'>
-                    <Link to='/' className={`nav__home-link ${className}`}>
+                    <Link 
+                      to='/' 
+                      className={`nav__home-link ${className} ${isHovered ? 'nav__dynamic-links_on nav__home-on' : ''}`}
+                    >
                       Home
                     </Link>
-                    <Link to='/saved-news' className={`nav__saved-articles ${className}`}>
+                    <Link 
+                      to='/saved-news' 
+                      className={`nav__saved-articles ${className} ${isHovered ? 'nav__dynamic-links_on' : ''}`}
+                    >
                       Saved Articles
                     </Link>
-                    <button className={!isHome ? 'nav__user-button-saved' :`nav__user-button`}>
+                    <button 
+                      className={!isHome ? 'nav__user-button-saved' :`nav__user-button`}
+                    >
                       <div className='nav__user-div'>
-                        <span className={`nav__user-text ${className}`}>{currentUser.user}</span> 
+                        <span className={`nav__user-text ${className}`} onClick={handleMobileClick} >{currentUser.user}</span> 
                         <img 
                           src={!isHome ? 'src/assets/logout-dark.svg' : 'src/assets/logout-icon.svg'} 
                           className='nav__user-button_img' 
