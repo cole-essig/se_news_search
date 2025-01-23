74417887b73ac5f32ab8bff3d32bd42c89638600
Cole Essig <hoopwalker@yahoo.com>
2025-01-22 22:40:00 -0700

    Complete revamp of nav component, now incorpated more smooth mobile structure and settings. Needs slight tweak for saved articles nav, styles updated

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,65 +4,122 @@ import { Link } from 'react-router-dom';
 import { useContext } from "react";
 import { CurrentUserContext } from "../../utils/contexts/CurrentUserContext";
 
-function NavBar({ isLoggedIn, signOut, className, isHome, signInClick }) {
+function NavBar({ isLoggedIn, signOut, isHome, signInClick }) {
     const currentUser = useContext(CurrentUserContext);
-    const [isHovered, setIsHovered] = useState(false);
-    const handleMouseEnter = () => setIsHovered(true);
-    const handleMouseLeave = () => setIsHovered(false);
+    const [screenSize, setScreenSize] = useState(window.innerWidth < 600);
+    const [dropDown, setDropDown] = useState(false);
     
     const handleLogout = (e) => {
       e.preventDefault();
       signOut();
-    }
+    };
 
     const handleSignIn = () => {
-    signInClick();
+      signInClick();
+    };
+
+    const handleDropDownClick = () => {
+      setDropDown(!dropDown);
     }
 
-    const handleMobileClick = () => {
-      setIsHovered(!isHovered)
-    }
-
+    useEffect(() => {
+      const handleResize = () => {
+        setScreenSize(window.innerWidth < 600);
+      };
+      window.addEventListener('resize', handleResize);
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }, []);
     return(
-        <nav className='nav'>
-                <div className='nav__static-link'>
-                  <Link to='/' className={`nav__logo-home ${className}`}>
-                    NewsExplorer
-                  </Link>
-                </div>
-                {!isLoggedIn ?
-                  <div className='nav__dynamic-links'>
+        <nav className={`nav ${screenSize && dropDown ? 'dropdown_black' : ''}`}>
+            <div className='nav__static-link'>
+              <Link to='/' className={`nav__logo-home`}>
+                NewsExplorer
+              </Link>
+            </div>
+            {screenSize ? (
+              // Mobile version
+              <>
+                <button className='nav__dropdown_mobile-button' onClick={handleDropDownClick}></button>
+                {!isLoggedIn ? (dropDown &&
+                    <div className="nav__dropdown">
+                    <Link to='/' className={`nav__home-link_mobile`}>
+                      Home
+                    </Link>
+                    <button 
+                      type='button' 
+                      className={`nav__button_mobile`} 
+                      onClick={handleSignIn} 
+                    >Sign In</button>
+                    </div>
+                ) : (dropDown &&
+                  <div className="nav__dropdown">
+                     <Link to='/' className={`nav__home-link_mobile`}>
+                      Home
+                    </Link>
                     <Link 
-                      to='/' 
-                      className={`nav__home-link`}
+                      to='/saved-news' 
+                      className='nav__saved-articles_mobile'
                     >
+                      Saved Articles
+                    </Link>
+                    <button 
+                      className={!isHome ? 'nav__user-button-saved_mobile' : `nav__user-button_mobile`}
+                    >
+                      <div className='nav__user-div_mobile'>
+                        <span className={`nav__user-text`}>
+                          {currentUser.user}
+                        </span>
+                        <img 
+                          src='src/assets/logout-icon.svg' 
+                          className='nav__user-button_img' 
+                          onClick={handleLogout} 
+                        />
+                      </div>
+                    </button>
+                  </div>
+                )}
+              </>
+            ) : (
+              // Desktop version
+              <div className='nav__dynamic-links'>
+                {!isLoggedIn ? (
+                  <>
+                    <Link to='/' className={`nav__home-link`}>
                       Home
                     </Link>
                     <button 
                       type='button' 
                       className={`nav__button`} 
                       onClick={handleSignIn} 
-                    >Sign In</button>
-                  </div>
-                  :
-                  <div className='nav__dynamic-links'>
+                    >
+                      Sign In
+                    </button>
+                  </>
+                ) : (
+                  <>
                     <Link 
                       to='/' 
-                      className={`nav__home-link ${className} ${isHovered ? 'nav__dynamic-links_on nav__home-on' : ''}`}
+                      className={`nav__home-link`}
                     >
                       Home
                     </Link>
                     <Link 
                       to='/saved-news' 
-                      className={`nav__saved-articles ${className} ${isHovered ? 'nav__dynamic-links_on' : ''}`}
+                      className={`nav__saved-articles`}
                     >
                       Saved Articles
                     </Link>
                     <button 
-                      className={!isHome ? 'nav__user-button-saved' :`nav__user-button`}
+                      className={!isHome ? 'nav__user-button-saved' : `nav__user-button`}
                     >
                       <div className='nav__user-div'>
-                        <span className={`nav__user-text ${className}`} onClick={handleMobileClick} >{currentUser.user}</span> 
+                        <span 
+                          className={`nav__user-text`} 
+                        >
+                          {currentUser.user}
+                        </span> 
                         <img 
                           src={!isHome ? 'src/assets/logout-dark.svg' : 'src/assets/logout-icon.svg'} 
                           className='nav__user-button_img' 
@@ -70,10 +127,12 @@ function NavBar({ isLoggedIn, signOut, className, isHome, signInClick }) {
                         />
                       </div>
                     </button>
-                  </div>
-                }
-              </nav>
-    )
-};
+                  </>
+                )}
+              </div>
+            )}
+        </nav>
+    );
+}
 
 export default NavBar;
